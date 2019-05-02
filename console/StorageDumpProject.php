fee089fc391b79bbbf7bd02d562ea9385cbe5df3
Gonzalo Henríquez <henriquez@wiz.cl>
2019-05-01 23:46:30 -0400

    fire() is now handle()

--- console/StorageDumpProject.php
+++ console/StorageDumpProject.php
@@ -20,7 +20,7 @@ class StorageDumpProject extends Command
      * Execute the console command.
      * @return void
      */
-    public function fire()
+    public function handle()
     {
         $this->info(trans('genius.storageclear::lang.project.cleaning'));
 
