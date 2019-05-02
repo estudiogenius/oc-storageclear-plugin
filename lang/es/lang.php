81d736deeb1f8a6661f20c3e90c2f020aa3567bd
Gonzalo Henríquez <henriquez@wiz.cl>
2019-05-01 23:46:07 -0400

    Spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Storage Cleaner',
+        'description' => 'Busca en el directorio Uploads archivos desconectados de la base de datos.',
+    ],
+    'clear' => [
+        'seeking' => [
+            'files' => 'Buscando archivos no registrados...',
+            'registers' => 'Buscando registros sin archivos o sin modelos relacionados...',
+            'directories' => 'Buscando directorios vacíos...'
+        ],
+        'removed' => [
+            'files' => '... :count archivos eliminados, :total encontrados!',
+            'registers' => '... :count registros eliminados, :total encontrados!',
+            'directories' => '... :count directorios vacíos eliminados, :total encontrados!'
+        ]
+    ],
+    'project' => [
+        'cleaning' => 'Realizando un limpieza rápida para generar un archivo zip pequeño...',
+        'start' => 'Construyendo un archivo zip para tu increíble proyecto...',
+        'end' => '... Listo! El archivo zip se encuentra en la raíz del proyecto: :file'
+    ],
+    'database' => [
+        'start' => 'Realizando un respaldo de la base de datos...',
+        'end' => '... Listo! El archivo de respaldo se encuentra en la raíz del proyecto: :file'
+    ]
+];
